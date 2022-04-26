5548fb43584b5fe7495c6318e8e54b5f9cc5a321
Dmitry Khudyakov <dmitry.khudyakov@bytepace.com>
2022-04-26 09:16:44 +0600

    Added LoadingOverlay and hash256 (iOS13)

---
+++ MyPlaygrounds/LoadingOverlay.swift
@@ -0,0 +1,75 @@
+//
+//  LoadingOverlay.swift
+//  MyPlaygrounds
+//
+//  Created by Dmitry on 26.04.2022.
+//
+
+import UIKit
+import PinLayout
+
+public class LoadingOverlay {
+    var loader = LoaderView()
+
+    class var shared: LoadingOverlay {
+        struct Static {
+            static let instance: LoadingOverlay = LoadingOverlay()
+        }
+        return Static.instance
+    }
+
+    public func show(in view: UIView) {
+        loader.removeFromSuperview()
+        loader.frame = view.bounds
+        view.addSubview(loader)
+        loader.activityIndicator.startAnimating()
+    }
+
+    public func hide() {
+        loader.activityIndicator.stopAnimating()
+        loader.removeFromSuperview()
+    }
+}
+
+class LoaderView: UIView {
+    let overlayView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.black
+        view.layer.cornerRadius = 10
+        return view
+    }()
+    
+    let activityIndicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView()
+        indicator.style = UIActivityIndicatorView.Style.large
+        return indicator
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubviews(overlayView)
+        overlayView.addSubview(activityIndicator)
+        
+        backgroundColor = UIColor("242424", alpha: 0.4)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        overlayView.pin
+            .width(80)
+            .height(80)
+            .vCenter()
+            .hCenter()
+        
+        activityIndicator.pin
+            .width(40)
+            .height(40)
+            .vCenter()
+            .hCenter()
+    }
+}
--- MyPlaygrounds/MyExtensions/String+Extension.swift
+++ MyPlaygrounds/MyExtensions/String+Extension.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import CryptoKit
 
 extension String {
     var containsWhitespace : Bool {
@@ -18,5 +19,13 @@ extension String {
             return UIApplication.shared.canOpenURL(url as URL)
         }
         return false
+    }
+    
+    func hash256() -> String {
+        // iOS 13.0
+        let inputData = Data(utf8)
+        
+        let hashed = SHA256.hash(data: inputData)
+        return hashed.compactMap { String(format: "%02x", $0) }.joined()
     }
 }
