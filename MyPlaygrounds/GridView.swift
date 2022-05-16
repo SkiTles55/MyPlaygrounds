c72a361b1aac811bb2786f725d5ade78d57c9dc2
Dmitry Khudyakov <dmitry.khudyakov@bytepace.com>
2022-05-16 11:13:09 +0600

    GridView update

--- MyPlaygrounds/GridView.swift
+++ MyPlaygrounds/GridView.swift
@@ -7,28 +7,32 @@
 
 import UIKit
 
-class GridView: UIView
-{
+class GridView: UIView {
     private var path = UIBezierPath()
+    private var gridColor = UIColor.lightGray
     var gridWidthMultiple: CGFloat = 25
     var gridHeightMultiple: CGFloat = 25
     var startX: CGFloat = 0
     var startY: CGFloat = 0
 
-    fileprivate var gridWidth: CGFloat
-    {
-        return bounds.width/CGFloat(gridWidthMultiple)
+    fileprivate var gridWidth: CGFloat {
+        return (bounds.width - startX) / CGFloat(gridWidthMultiple)
     }
 
-    fileprivate var gridHeight: CGFloat
-    {
-        return bounds.height/CGFloat(gridHeightMultiple)
+    fileprivate var gridHeight: CGFloat {
+        return (bounds.height - startY) / CGFloat(gridHeightMultiple)
+    }
+    
+    fileprivate var labelAttributes: [NSAttributedString.Key : NSObject] {
+        return [
+            NSAttributedString.Key.font : UIFont.systemFont(ofSize: 10),
+            NSAttributedString.Key.foregroundColor : gridColor
+        ]
     }
 
-    fileprivate func drawGrid()
-    {
+    fileprivate func drawGrid() {
         path = UIBezierPath()
-        path.lineWidth = 1.0
+        path.lineWidth = 0.5
 
         for index in 0...Int(gridWidthMultiple)
         {
@@ -36,6 +40,9 @@ class GridView: UIView
             let end = CGPoint(x: (CGFloat(index) * gridWidth) + startX, y: bounds.height - startY)
             path.move(to: start)
             path.addLine(to: end)
+            if (gridWidthMultiple > 20 && index % 3 == 0) || gridWidthMultiple <= 20 {
+                drawXLabel(text: index == 0 ? "X" : "\(index)", point: end)
+            }
         }
         
         for index in 0...Int(gridHeightMultiple) {
@@ -43,15 +50,35 @@ class GridView: UIView
             let end = CGPoint(x: bounds.width, y: bounds.height - ((CGFloat(index) * gridHeight) + startY))
             path.move(to: start)
             path.addLine(to: end)
+            if (gridHeightMultiple > 10 && index % 2 == 0) || gridHeightMultiple <= 10 {
+                drawYLabel(text: index == 0 ? "Y" : "\(index)", point: start)
+            }
         }
         
         path.close()
     }
+    
+    private func drawXLabel(text: String, point: CGPoint) {
+        let string = text as NSString
+        let stringSize = string.size(withAttributes: labelAttributes)
+        string.draw(
+            in: CGRect(x: point.x - (stringSize.width / 2), y: point.y + 3, width: stringSize.width, height: stringSize.height),
+            withAttributes: labelAttributes
+        )
+    }
+    
+    private func drawYLabel(text: String, point: CGPoint) {
+        let string = text as NSString
+        let stringSize = string.size(withAttributes: labelAttributes)
+        string.draw(
+            in: CGRect(x: point.x - stringSize.width - 3, y: point.y - (stringSize.height / 2), width: stringSize.width, height: stringSize.height),
+            withAttributes: labelAttributes
+        )
+    }
 
-    override func draw(_ rect: CGRect)
-    {
+    override func draw(_ rect: CGRect) {
         drawGrid()
-        UIColor.white.setStroke()
+        gridColor.setStroke()
         path.stroke()
     }
 }
