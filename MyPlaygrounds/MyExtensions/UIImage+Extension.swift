d430c2782629f6f7c96d63681b9c65f43ec061b0
Dmitry Khudyakov <dmitry.khudyakov@bytepace.com>
2022-06-07 14:58:35 +0600

    Added UIImage extension sliderThumbImage

--- MyPlaygrounds/MyExtensions/UIImage+Extension.swift
+++ MyPlaygrounds/MyExtensions/UIImage+Extension.swift
@@ -23,4 +23,23 @@ extension UIImage {
 
         return img!
     }
+    
+    class func sliderThumbImage(color: UIColor, borderColor: UIColor, size: CGFloat = 20, borderWidth: CGFloat = 2) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: size, height: size), false, 0)
+        guard let ctx = UIGraphicsGetCurrentContext() else { return nil }
+        ctx.saveGState()
+        
+        ctx.setFillColor(color.cgColor)
+        ctx.setStrokeColor(borderColor.cgColor)
+        ctx.setLineWidth(borderWidth)
+        
+        ctx.addEllipse(in: CGRect(x: borderWidth / 2, y: borderWidth / 2, width: size - borderWidth, height: size - borderWidth))
+        ctx.drawPath(using: .fillStroke)
+        
+        ctx.restoreGState()
+        let img = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return img
+    }
 }
